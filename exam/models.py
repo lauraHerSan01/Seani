8521675f8907fef6bd21c3551b4e24df84ff41c9
laura <hernandez.sandoval.laura@gmail.com>
2024-02-29 14:13:04 -0600

    Seani 1

--- exam/models.py
+++ exam/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+from career.models import Career
+from library.models import Module, Question
+
 
 # Create your models here.
 class Stage(models.Model):
@@ -24,3 +29,68 @@ class Stage(models.Model):
     class Meta:
         verbose_name = "etapa"
         verbose_name_plural = "etapas"
+        
+class Exam(models.Model):
+    user = models.OneToOneField(
+        User, 
+        on_delete=models.CASCADE,
+        verbose_name = "Usuario")
+    career = models.ForeignKey(
+        Career,
+        on_delete=models.CASCADE,
+        verbose_name = "Carrera")
+    stage = models.ForeignKey(
+        Stage, 
+        on_delete=models.CASCADE,
+        verbose_name = "Etapa")
+    modules = models.ManyToManyField(
+            Module,  
+            through='ExamModule' 
+    )
+    questions = models.ManyToManyField(
+        Question,
+        through='Breakdown'
+    )
+    score = models.FloatField(
+        verbose_name = "Calificacion",
+        default = 0.0
+    )
+    created = models.DateTimeField(
+        verbose_name = "Fecha de Creacion",
+        auto_now_add=True)
+    update = models.DateField(
+        auto_now=True,
+        verbose_name = "Fecha de actualizacion"
+    )
+    
+    def set_modules(self):
+        for module in Module.objects.all():
+            self.modules.add(module)
+    
+    def set_questions(self):
+        for module in self.modules.all():
+            for question in module.question_set.all():
+               # self.questions.add(question)
+                Breakdown.objects.create(
+                    exam= self,
+                    question = question,
+                    correct = question.correct)
+    
+    def __str__(self):
+        return f"{self.user.username} - {self.score}"
+    
+    class Meta:
+        verbose_name = "Examen"
+        verbose_name_plural = "Examenes"
+
+class ExamModule(models.Model):
+    exam = models.ForeignKey(Exam, on_delete=models.CASCADE)
+    module = models.ForeignKey(Module, on_delete=models.CASCADE)
+    active = models.BooleanField(default = True)
+    score = models.FloatField(default=0.0)
+
+class Breakdown (models.Model):
+    exam = models.ForeignKey(Exam, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer = models.CharField(max_length=5, default='-')
+    correct = models.CharField(max_length=5, default='-')
--- exam/views.py
+++ exam/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Stage, Exam
+from django.contrib.auth.models import User
+from career.models import Career
 # Create your views here.
+def create(request):
+    stage = Stage.objects.get(pk=1)
+    career = Career.objects.get(pk=1)
+    user = User.objects.create(
+                    username = 'tres',
+                    password='tres')
+    exam = Exam.objects.create(user = user,
+                              stage = stage,
+                              career = career)
+    exam.set_modules()
+    exam.set_questions()
+    return HttpResponse('Usuario y examen creado ')
+    
